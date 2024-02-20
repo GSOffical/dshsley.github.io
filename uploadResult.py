ffdc14013d418361938e221890b8ba39362451c2
GSOffical <160608329+GSOffical@users.noreply.github.com>
2024-02-21 06:55:16 +0800

    Update uploadResult.py

--- uploadResult.py
+++ uploadResult.py
@@ -1,18 +1,19 @@
 import requests
 import os
+import sys
+from urllib.parse import unquote
 
-def upload_result(name, loved_one, probability):
+def upload_result(name, loved_person, probability):
     token = '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'
-    headers = {
-        'Authorization': f'token {token}',
-        'Content-Type': 'application/json'
-    }
+    headers = {'Authorization': f'token {token}'}
+    url = 'https://api.github.com/repos/GSOffical/dshsley.github.io/contents/.result'
+
+    content = f'{name},{loved_person},{probability}'
     data = {
-        'name': name,
-        'loved_one': loved_one,
-        'probability': probability
+        'message': 'Upload test result',
+        'content': content.encode('utf-8').hex(),
     }
-    url = 'https://raw.githubusercontent.com/GSOffical/dshsley.github.io/main/.result'
+
     response = requests.put(url, headers=headers, json=data)
     if response.status_code == 201:
         print('上传成功')
@@ -20,7 +21,8 @@ def upload_result(name, loved_one, probability):
         print('上传失败')
 
 if __name__ == '__main__':
-    name = input('请输入你的名字：')
-    loved_one = input('请输入你喜欢的人：')
-    probability = input('请输入概率：')
-    upload_result(name, loved_one, probability)
+    query = unquote(sys.argv[1]).split('?')[1]
+    name = query.split('&')[0].split('=')[1]
+    loved_person = query.split('&')[1].split('=')[1]
+    probability = query.split('&')[2].split('=')[1]
+    upload_result(name, loved_person, probability)
